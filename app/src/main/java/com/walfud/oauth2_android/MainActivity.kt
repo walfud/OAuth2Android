19e48bc8bf0c1cb4ee290c070f0135a7bfc21c57
walfud <hi@walfud.com>
2017-05-24 23:38:43 +0700

    注册登陆

--- app/src/main/java/com/walfud/oauth2_android/MainActivity.kt
+++ app/src/main/java/com/walfud/oauth2_android/MainActivity.kt
@@ -3,14 +3,19 @@ package com.walfud.oauth2_android
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.walfud.walle.algorithm.hash.HashUtils
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
 import okhttp3.HttpUrl
 import okhttp3.MediaType
 import okhttp3.Request
 import okhttp3.RequestBody
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.json.JSONObject
 import java.io.Serializable
 
 
@@ -45,7 +50,7 @@ class MainActivity : Activity(), AnkoLogger {
                 .execute()
         if (!loginResponse.isSuccessful) throw RuntimeException("login fail")
 
-        return Gson().fromJson(loginResponse.body().string(), LoginResponseBean::class.java)
+        return getResponse(loginResponse.body().string(), LoginResponseBean::class.java)
     }
 
     fun user(token: String): UserResponseBean {
@@ -54,14 +59,14 @@ class MainActivity : Activity(), AnkoLogger {
                         .scheme("http")
                         .host("oauth2.walfud.com")
                         .addPathSegment("user")
-                        .addQueryParameter("token", token)
                         .build())
                 .get()
+                .header("X-Access-Token", token)
                 .build())
                 .execute()
         if (!userResponse.isSuccessful) throw RuntimeException("fetch user fail")
 
-        return Gson().fromJson(userResponse.body().string(), UserResponseBean::class.java)
+        return getResponse(userResponse.body().string(), UserResponseBean::class.java)
     }
 }
 
@@ -72,20 +77,39 @@ class MainActivityUI : AnkoComponent<MainActivity> {
             val password = editText()
             button(R.string.main_login) {
                 onClick {
-//                    with(ui.owner) {
-//                        val userResponseBean = async(CommonPool) {
-//                            val loginResponseBean = login(username.text.toString(), password.text.toString())
-//                            user(loginResponseBean.token)
-//                        }
-//                        toast(userResponseBean.await().oid)
-//                    }
-                    OAuth2Activity.startActivityForResult(ui.owner, "")
+                    with(ui.owner) {
+                        val userResponse = async(CommonPool) {
+                            val loginResponseBean = login(username.text.toString(), HashUtils.md5(password.text.toString()))
+                            user(loginResponseBean.accessToken)
+                        }
+                        try {
+                            val userResponseBean = userResponse.await()
+                            toast(userResponseBean.oid)
+                        } catch (e: Exception) {
+                            toast(e.message?:"Unknown Error")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+fun <T> getResponse(responseBody: String, clazz: Class<T>): T {
+    val json = JSONObject(responseBody)
+    val err = json.optString("err")
+    if (TextUtils.isEmpty(err)) {
+        return Gson().fromJson(responseBody, clazz)
+    }
+
+    throw RuntimeException(err)
+}
+
+data class ErrorResponse(val err: String?)
 data class LoginRequestBean(val username: String, val password: String)
-data class LoginResponseBean(val token: String, val refresh_token: String)
+data class LoginResponseBean(
+        @SerializedName("access_token") val accessToken: String,
+        @SerializedName("refresh_token") val refreshToken: String
+)
+
 data class UserResponseBean(val oid: String) : Serializable
